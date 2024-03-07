252f531a48aac82993d6f7275e6c4f78ba5aa0c5
kjcabPL <kcabrera@profoundlogic.com>
2024-03-07 17:56:05 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,44 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+var port = 8080;
+
+// file entries & directories
+var maindir = "./pages";
+var infodir = "/info";
+var indexpage = "/index";
+
+// Server Object
+http.createServer(function(req, res) {
+  let q = url.parse(req.url, true);
+  let filename = "";
+  let dir = "./pages/";
+
+  // routing starts here
+  if (q.pathname !== "/") {
+    if (q.pathname === "/index") { filename = maindir + indexpage; }
+    else {
+      if (q.pathname === "/about" || q.pathname === "/resume") 
+      filename = maindir + infodir + q.pathname;
+    }
+  }
+  else { filename = maindir + indexpage; } // default to main page instead
+  filename += ".html"; // hide URL filetype by forcing file type addition to filename
+  
+  // start with the index.html
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        console.log(err);
+        return res.end("404 Not Found");
+
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    console.log("Incoming Request: " + req.url);
+    return res.end();
+  });
+  
+}).listen(port);
+
+console.log("Server now listening at port " + port );
