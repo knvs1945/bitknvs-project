4d12205d65933a7407e34465877d6ce5f4915cbb
kjcabPL <kcabrera@profoundlogic.com>
2024-05-21 16:43:16 +0800

    - Add SQLite package to project for highscore recording

--- pages/data/spykeball/hs.js
+++ pages/data/spykeball/hs.js
@@ -2,8 +2,55 @@ const fs = require("fs");
 const readLine = require("readline");
 const path = require("path");
 
-
+// add sqlite module for writing data
+const sqlite3 = require('sqlite3').verbose();
 const filename = path.join(__dirname, "SBDAT.csv");
+
+// create and check for highscore DB
+console.log("Running DB Check at: " + path.join(__dirname, '/spykeball.sqlite3'));
+let db = new sqlite3.Database(path.join(__dirname, '/spykeball.sqlite3'), (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    else console.log('Spykeball Database accessed');
+} );
+
+let hsTableUL = "highscoresUL";
+let hsTableTA = "highscoresTA";
+
+// general question for running queries
+function runQuery(query, callback) {
+    db.run(query, callback);
+}
+
+// check for both Unlimited and Time Attack tables
+function checkTables(tableName) {
+    // check if our table already exists:
+    let query = "";
+    query = `SELECT * FROM sqlite_master WHERE type='table' AND name='${tableName}'`;
+    db.get(query, (err, row) => {
+        if (err) {
+            console.error(err.message);
+        }
+        else if (row) {
+            console.log("Highscore table found: " + tableName);
+        }
+        else {
+            console.log("Highscore table does not exist. Creating table: " + tableName );
+            if (tableName === hsTableUL) query = `CREATE TABLE ${tableName} ( id INTEGER PRIMARY KEY, name TEXT, score INTEGER, targets INTEGER, date DATE )`;
+            else if (tableName === hsTableTA) query = `CREATE TABLE ${tableName} ( id INTEGER PRIMARY KEY, name TEXT, score INTEGER, targets INTEGER, time TIME, date DATE )`;
+            runQuery(query);
+        }
+    });
+}
+
+// initiate DB and check if highscores table already exists.
+function startDB() {
+    checkTables(hsTableUL);
+    checkTables(hsTableTA);
+}
+
+// check if 
 
 // read HS data here 
 async function readHS() {
@@ -75,10 +122,16 @@ async function fileRead(filename, rowID = 0) {
 }
 
 // write the file here
+// To Post data, must use this body structure:
+// '{"rank":"2", "name":"KJC", "targets":"5", "score":"5", "time":"3.5", "date":"3/5"}'
+// Sample Curl command
+// curl -X POST -d '{"rank":"2", "name":"KJC", "targets":"5", "score":"5", "time":"3.5", "date":"3/5"}' http://localhost:5000/sbupdatehs
+
 async function fileWrite(filename, data) {
     const lineData = [];
     let fileLength = 0;
     let lTemp = null;
+    var scoreArray = [];
     let strUpdated = "";
 
     const fileStream = fs.createReadStream(filename);
@@ -90,11 +143,18 @@ async function fileWrite(filename, data) {
     // get the data from the file by reading line by line
     for await (const line of lineReader) {
         fileLength++;
+        scoreArray.push(line);
+        console.log("Length: " + scoreArray.length);
         strUpdated += line + "\n";
     }
 
-    data = JSON.stringify(data);
-    strUpdated += data; // add to end of line
+    // Then, sort the new data to be added into the 
+    scoreArray = sortHSData(scoreArray, data);
+
+    // data = JSON.stringify(data);
+    // add to end of line
+    console.log(data);
+    strUpdated += data.rank + "," + data.name + "," + data.targets + "," + data.score + "," + data.time + "," + data.date; 
     lineReader.close();
     fileStream.close();
 
@@ -108,6 +168,21 @@ async function fileWrite(filename, data) {
 
     return message;
 }
+
+// sort through the score list here and place the new data
+async function sortHSData(scoreList, newScore) {
+
+    console.log("Sorting data through score list: " + scoreList.length);
+    for (var i = 0; i < scoreList.length; i++) {
+        console.log("Score " + i + ":");
+        console.log(scoreList);
+    }
+    console.log(newScore);
+
+    return scoreList;
+}
+
+startDB();
 
 exports.readHS = readHS;
 exports.recordHS = recordHS;
