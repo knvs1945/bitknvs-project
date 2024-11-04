5e0cc19096619882fa304b6e2e8ddffb18998982
kjcabPL <kcabrera@profoundlogic.com>
2024-11-05 00:07:49 +0800

    - Add spykeball database get and pull routes to express server

--- server_express.js
+++ server_express.js
@@ -43,18 +43,140 @@ function loadIndex(req, res) {
   res.sendFile(fullPath);
 }
 
+// load page parts like navbar and footer
 function loadParts(req, res) {
   if (setupCORS()) return;
   let fullPath = path.join(__dirname, req.path);
   res.sendFile(fullPath);
 }
 
+// load page contents
 function loadContent(req, res) {
   if (setupCORS()) return;
   let pageloc = req.path;    
   let fullPath = path.join(__dirname, maindir, infodir, pageloc + ".html");
   console.log("Redirecting to: ", fullPath );
   res.sendFile(fullPath);
+}
+
+// database get content
+function loadGetDB(req, res) {
+  if (setupCORS()) return;
+  let q = url.parse(req.url, true);
+  let urlPath = req.path;
+  let urlParts = urlPath.split('/');
+  let queryParts = { gameMode: null, column: null, order: null, page: null, limit: null };
+  queryParts.gameMode = q.query.mode;
+  queryParts.column = q.query.sort;
+  queryParts.order = q.query.order;
+  queryParts.page = parseInt(q.query.page);
+  queryParts.limit = parseInt(q.query.limit);
+
+  // reject request if gameMode is not present or can't be identified
+  if (!queryParts.gameMode || (queryParts.gameMode !== "ul" && queryParts.gameMode !== "ta")) {
+    res.writeHead(404, {
+      'Content-Type': "text/html",
+      'Access-Control-Allow-Origin': "*",
+      'Access-Control-Allow-Methods': "GET, POST, OPTIONS",
+      'Access-Control-Allow-Headers': "Content-Type"
+    });
+    res.write("Error: No Game Mode Specified");
+    res.end();
+  }
+  else dbGetSpykeballData(req, res, queryParts);
+  
+}
+
+
+// database post content
+function loadPostDB(req, res) {
+  if (setupCORS()) return;
+  let urlPath = req.path;
+  let urlParts = urlPath.split('/');
+  // reject request if table is not present or can't be identified
+  if (urlParts[2] || (urlParts[2] !== "ul" && urlParts[2] !== "ta")) {
+    console.log("RecordHS Table ID unidentified: ", urlParts);
+    res.writeHead(404, {
+      'Content-Type': "text/html",
+      'Access-Control-Allow-Origin': "*",
+      'Access-Control-Allow-Methods': "GET, POST, OPTIONS",
+      'Access-Control-Allow-Headers': "Content-Type"
+    });
+    res.write("RecordHS: TableID not found");
+    res.end();
+  }
+  else {
+    if (urlParts.length > 1) {
+      switch(urlParts[1]) {
+        case "sbupdatehs": 
+          let body = "";
+          req.on('data', chunk => {
+            body += chunk.toString();
+          });
+          req.on('end', dbPostSpykeballData(req, res, urlParts[2]), body);
+          break;
+      }
+    }
+  }
+}
+
+// Spykeball DB actions
+function dbGetSpykeballData (req, res, queryParts) {
+
+  // query parameter checks and defaults
+  if (!queryParts.column || queryParts.column !== "targets" && queryParts.column !== "time" && queryParts.column !== "name" && queryParts.column !== "date") queryParts.column = "score";
+  else if (queryParts.column == "ul" && queryParts.column == "time") queryParts.column = "score";
+  queryParts.order = (queryParts.order && (queryParts.order == "asc" || queryParts.order == "desc")) ? queryParts.order : "desc";
+  if (queryParts.gameMode == "ul") queryParts.gameMode = "unlimited";
+  else if (queryParts.gameMode == "ta") queryParts.gameMode = "time attack";
+  if (isNaN(queryParts.page) || !Number.isInteger(queryParts.page) || queryParts.page < 1) queryParts.page = 1;
+  if (isNaN(queryParts.limit) || !Number.isInteger(queryParts.limit) || queryParts.limit < 1) queryParts.limit = 10;
+
+  let result = SBAPI_readHS.readHS(queryParts.gameMode, queryParts.column, queryParts.order, queryParts.page, queryParts.limit);
+  if (typeof result === 'string') dbSendResult(res, 404, 'text/html', result);
+  else {
+    Promise.resolve(result)
+    .then(data => {
+      let returnData = {
+        highscores: data.data,
+        length: data.length
+      };
+      dbSendResult(res, 200, 'application/json', JSON.stringify(returnData, null, 2));
+    })
+    .catch(err => {
+      dbSendResult(res, 404, 'text/html', err);
+    })
+  }
+}
+
+function dbPostSpykeballData (req, res, tableName, data) {
+  console.log(data);
+  let postData = JSON.parse(data);
+  let gameMode = "unlimited";
+  if (tableName == "ul") gameMode = "unlimited";
+  else if (tableName == "ta") gameMode = "time attack";
+  console.log("Updating table: " + gameMode + " with data: ", postData);
+
+  let result = SBAPI_readHS.recordHS(gameMode, postData);
+  if (typeof result === "string") dbSendResult(res, 200, 'text/plain', result);
+  else {
+    result.then(data => {
+      dbSendResult(res, 200, 'application/json', JSON.stringify(data, null, 2));
+    });    
+  }
+}
+
+// send results from db access
+function dbSendResult(res, id, contentType, result) {
+  res.writeHead(id, {
+    'Content-Type': contentType,
+    'Access-Control-Allow-Origin': "*",
+    'Access-Control-Allow-Methods': "GET, POST, OPTIONS",
+    'Access-Control-Allow-Headers': "Content-Type"
+  });
+  res.write(result);
+  res.end();
+  
 }
 
 // GET routes
@@ -72,9 +194,13 @@ app.get('/resume', loadContent);
 app.get('/projects', loadContent);
 app.get('/downloads', loadContent);
 
+app.get('/sbreadhs*', loadGetDB);
+
 // POST routes
 app.post('/index', loadIndex);
 app.post('/', loadIndex);
+
+app.post('/sbupdatehs/*', loadPostDB);
 
 // OPTION routes
 app.options('/', (req, res)=> {
