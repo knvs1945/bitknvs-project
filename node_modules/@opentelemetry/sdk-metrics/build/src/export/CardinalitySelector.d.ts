07384a3fb46257d3bb2fd1a8492670748baa16b6
kjcabPL <kcabrera@profoundlogic.com>
2025-02-25 19:12:23 +0800

    Adding Artillery package to dev dependency

---
+++ node_modules/@opentelemetry/sdk-metrics/build/src/export/CardinalitySelector.d.ts
@@ -0,0 +1,6 @@
+import { InstrumentType } from '../InstrumentDescriptor';
+/**
+ * Cardinality Limit selector based on metric instrument types.
+ */
+export declare type CardinalitySelector = (instrumentType: InstrumentType) => number;
+//# sourceMappingURL=CardinalitySelector.d.ts.map
