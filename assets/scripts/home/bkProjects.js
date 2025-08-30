ddfc70fcebadd3a53abe4ca542b005c5620abe61
kjcabPL <kcabrera@profoundlogic.com>
2025-08-31 01:06:11 +0800

    - Add logic to reading project detail logic to bkProjects.js and update projects3.html look to include scroll locking

--- assets/scripts/home/bkProjects.js
+++ assets/scripts/home/bkProjects.js
@@ -1,19 +1,37 @@
 let activeDiv = null
 let mainDiv = null
 let parentMainDiv = null
+let projectDetails = null
 
 function getTopDiv() {
-
   mainDiv = $("#projectsMain");
   parentMainDiv = $(mainDiv).parent();
   firstChild = mainDiv.children().first(); 
+}
 
+function getProjectDetails() {
+
+  $.getJSON("/assets/scripts/projects/projects_details.json", 
+    (data) => {
+      if (data) projectDetails = data;
+  })
+  .then(() => {
+    for (item in projectDetails) {
+      let itemID = "#" + item;
+      $(itemID).html(projectDetails[item].description);
+    }
+  })
+  .fail((error) => {
+    console.log("Error fetching json data: ", error);
+  });
+  
 }
 
 function expandProjectDiv(target) {
 
   let targetDiv = $("#"+ target)[0];
   let parentDiv = $(targetDiv).parent();
+  let btExpand = $(targetDiv).siblings(".bt_expand_down");
   if (!activeDiv) activeDiv = targetDiv;
   if (targetDiv.classList.contains("project_tab_hidden")) {
       
@@ -24,20 +42,26 @@ function expandProjectDiv(target) {
       scrollTop: scrollPos
     }, 500, () => {
       targetDiv.classList.remove("project_tab_hidden");
-      parentDiv.animate({
-        height: (parentMainDiv.height() - padding)
-      }, 500);
+      mainDiv[0].classList.remove("overflow-y-auto");
+      mainDiv[0].classList.add("overflow-y-hidden");
+      parentDiv.css("height", (parentMainDiv.height() - padding) + "px")
+      // parentDiv.animate({
+      //   height: (parentMainDiv.height() - padding)
+      // }, 500);
+      btExpand.css("transform", "rotate(180deg)");
     });
   }
   else {
       targetDiv.classList.add("project_tab_hidden");
       parentDiv.css("height", "");
+      mainDiv.css("overflow-y", "");
+      btExpand.css("transform", "");
+      mainDiv[0].classList.remove("overflow-y-hidden");
+      mainDiv[0].classList.add("overflow-y-auto");
       activeDiv = null
   }
  
-  // console.log(activeDiv)
-  // console.log(targetDiv)
-
 }
 
-getTopDiv()
+getTopDiv();
+getProjectDetails();
